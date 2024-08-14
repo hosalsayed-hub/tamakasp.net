53bc4786e962a6f2638f004bb5fe507f04e38bc5
hosalsayed-hub <hussam@tamaak.com>
2024-08-14 10:34:41 +0300

    first1
    first1

---
+++ Frontends/FreeCourse.Web/Controllers/AuthController.cs
@@ -0,0 +1,88 @@
+﻿using FreeCourse.Web.Models;
+using FreeCourse.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IIdentityService _identityService;
+        private readonly IUserCreateService _userCreateService;
+
+        public AuthController(IIdentityService identityService,IUserCreateService userCreateService)
+        {
+            _identityService = identityService;
+            _userCreateService = userCreateService;
+        }
+
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SigninInput signinInput)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var response = await _identityService.SignIn(signinInput);
+
+            if (!response.IsSuccessful)
+            {
+                response.Errors.ForEach(x =>
+                {
+                    ModelState.AddModelError(String.Empty, x);
+                });
+
+                return View();
+            }
+
+            return RedirectToAction(nameof(Index), "Home");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await _identityService.RevokeRefreshToken();
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SingUpInput singUpInput)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var response = await _userCreateService.SignUp(singUpInput);
+
+            if (!response.IsSuccessful)
+            {
+                response.Errors.ForEach(x =>
+                {
+                    ModelState.AddModelError(String.Empty, x);
+                });
+
+                return View();
+            }
+
+            return RedirectToAction(nameof(Index), "Home");
+        }
+    }
+}
---
+++ Frontends/FreeCourse.Web/Services/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using FreeCourse.Shared.Dtos;
+using FreeCourse.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<UserViewModel> GetUser();
+    }
+}
